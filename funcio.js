81e70d4cff4a76dbf74fab7f24221d2d69aa1fa0
Your Name <you@example.com>
2018-05-07 14:45:12 +0200

    logica i js

---
+++ funcio.js
@@ -0,0 +1,17 @@
+function datafinal (){
+    var datainici = document.getElementById('datainici').value;
+    var datafi = document.getElementById('datafi').value;
+    if (datainici > datafi){
+        alert ("La data inicial no pot ser major que la data final");
+        return false;
+    }else{
+        return true;
+    }
+}
+function diferencia (){
+    var data1 = document.getElementById('datainici').value;
+    var data2 = document.getElementById('datafi').value;
+    var dies = Math.floor((Date.parse(data2) - Date.parse(data1))/ 86400000);
+    dies = dies*(-1);
+        return dies;
+}
